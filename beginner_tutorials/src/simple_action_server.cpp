79f47ef7b64ae38d26f016aff802a7ab8e373862
Kentaro Wada <www.kentaro.wada@gmail.com>
2014-10-30 21:58:51 +0900

    finish documents distrubuted on 2014-10-8

---
+++ beginner_tutorials/src/simple_action_server.cpp
@@ -0,0 +1,21 @@
+#include <beginner_tutorials/DoDishesAction.h>
+#include <actionlib/server/simple_action_server.h>
+
+typedef actionlib::SimpleActionServer<beginner_tutorials::DoDishesAction> Server;
+
+void execute(const beginner_tutorials::DoDishesGoalConstPtr& goal, Server* as)
+{
+    as->setSucceeded();
+}
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "do_dishes_server");
+    ros::NodeHandle n;
+    Server server(n, "do_dishes", boost::bind(&execute, -1, &server), false);
+    server.start();
+
+    ros::spin();
+
+    return 0;
+}
