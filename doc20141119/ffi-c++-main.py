3e62259bc0fb801396b0bb82a00cc26ca55365cd
Kentaro Wada <www.kentaro.wada@gmail.com>
2014-12-03 11:46:35 +0900

    add: ffi-c++-main.py

---
+++ doc20141119/ffi-c++-main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+# ffi-python-main.py
+
+import ctypes
+ffi = ctypes.CDLL('./ffi-c++.so')
+
+ffi.sinc.argtypes = [ctypes.c_double]
+ffi.sinc.restype = ctypes.c_double
+print 'sin(1.0) = ', ffi.sinc(1.0)
